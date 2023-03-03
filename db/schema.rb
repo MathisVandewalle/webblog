af92e1a74ccafef45189007fccf337f5b46778a9
MathisVandewalle <mathis.vandewalle@mmibordeaux.com>
2023-03-03 16:22:56 +0100

    correction bug

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_03_132056) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_03_150100) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "chapo"
-    t.text "paragraph"
-    t.text "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "cards", force: :cascade do |t|
     t.string "title"
@@ -29,13 +20,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_132056) do
     t.text "attaque"
     t.text "description"
     t.text "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -50,18 +34,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_132056) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "uservs", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_uservs_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_uservs_on_reset_password_token", unique: true
   end
 
 end
